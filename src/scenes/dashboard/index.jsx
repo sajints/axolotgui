defa5556ba6da6f476547ba2f3c0e324ebec4a9d
sajints <sajints@gmail.com>
2024-02-22 21:11:49 +0100

    misc

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -9,7 +9,7 @@ import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
 import GeographyChart from "../../components/GeographyChart";
-import BarChart from "../../components/BarChart";
+// import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 
