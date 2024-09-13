24ab365ea3e39e7a75b742ef013ce729d518e353
srikant <srikanthdotin@gmail.com>
2024-09-13 18:26:59 +0530

    errorcheck3

--- src/scenes/healthMonitor/index.jsx
+++ src/scenes/healthMonitor/index.jsx
@@ -51,7 +51,7 @@ const HelathMonitor = () => {
     <Box m="20px">
       <Header title="Health Monitor" subtitle="Health Monitor Page" />
       <Typography>
-        {/* <HealthChart healthMonitorData/> */}
+        {/* <HealthChart healthMonitorData/> test*/}
         <CustomTable
           data={healthMonitorData}
           columns={columns}
