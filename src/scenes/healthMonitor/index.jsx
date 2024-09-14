308c467990ef66b22b9694cc14d5fbdfa9dba6ea
srikant <srikanthdotin@gmail.com>
2024-09-14 12:26:58 +0530

    errorcheck4

--- src/scenes/healthMonitor/index.jsx
+++ src/scenes/healthMonitor/index.jsx
@@ -1,21 +1,19 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, Typography, useTheme, TextField } from "@mui/material";
 import Header from "../../components/Header";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { tokens } from "../../theme";
-// import HealthChart from "../../components/health-chart";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { API_BASE_URL, GET_HEALTHMONITOR_URL } from "../../urlconstants";
 import CustomTable from "../../components/common-components/custom-table";
+// import HealthBarChart from "../../components/common-components/health-bar-chart";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
 
-const HelathMonitor = () => {
+const HealthMonitor = () => {
   const theme = useTheme();
   const [healthMonitorData, setHealthMonitorData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedDate, setSelectedDate] = useState(() => {
+    const today = new Date();
+    return today.toISOString().split("T")[0];
+  });
 
   const handleDateChange = (event) => {
     const date = event.target.value;
@@ -37,30 +35,139 @@ const HelathMonitor = () => {
       console.log(error, "error");
     }
   };
-  const columns = [
-    { field: "id", headerName: "ID" },
-    { field: "creationDate", headerName: "Created Date" },
-    { field: "result", headerName: "Result" },
-  ];
 
   useEffect(() => {
-    fetchData("");
-  }, []);
+    fetchData(`${selectedDate} 00:00:00`);
+  }, [selectedDate]);
+
+  const apiData = healthMonitorData.filter(
+    (item) => item.application === "Axolot API"
+  );
+  const sqlDbData = healthMonitorData.filter(
+    (item) => item.application === "Axolot SQLDB"
+  );
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 100,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "creationDate",
+      headerName: "Created Date",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+      valueGetter: (params) => params.value.split("T")[0],
+    },
+    {
+      field: "creationTime",
+      headerName: "Created Time",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+      valueGetter: (params) =>
+        params.row.creationDate.split("T")[1].split(".")[0],
+    },
+    {
+      field: "result",
+      headerName: "Result",
+      width: 100,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params) => (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: 24,
+            height: 24,
+            borderRadius: "50%",
+            backgroundColor: params.value === 1 ? "green" : "red",
+          }}
+        >
+          <FactCheckIcon
+            sx={{
+              color: "white",
+              fontSize: 16,
+            }}
+          />
+        </Box>
+      ),
+    },
+    {
+      field: "timeForResponse",
+      headerName: "Time for Response",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+    },
+  ];
 
   return (
     <Box m="20px">
       <Header title="Health Monitor" subtitle="Health Monitor Page" />
-      <Typography>
-        {/* <HealthChart healthMonitorData/> test1*/}
-        <CustomTable
-          data={healthMonitorData}
-          columns={columns}
-          dateFilter={selectedDate}
-          onDateChange={handleDateChange}
+
+      <Box display="flex" alignItems="center" mb="20px">
+        <TextField
+          type="date"
+          label="Select Date"
+          value={selectedDate}
+          onChange={handleDateChange}
+          InputLabelProps={{ shrink: true }}
+          sx={{
+            width: 220,
+            mr: 2,
+            "& .MuiInputBase-input": {
+              color: "white",
+            },
+            "& .MuiSvgIcon-root": {
+              color: "white",
+            },
+            "& .MuiFormLabel-root": {
+              color: "white",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "white",
+              },
+              "&:hover fieldset": {
+                borderColor: "white",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "white",
+              },
+              "& .MuiSvgIcon-root": {
+                color: "white",
+              },
+            },
+          }}
         />
-      </Typography>
+      </Box>
+
+      <Box display="flex" justifyContent="space-between" mb="20px">
+        <Box width="48%">
+          <Typography variant="h5" color={"#70d8bd"} gutterBottom>
+            Axolot API Data
+          </Typography>
+          <CustomTable data={apiData} columns={columns} />
+        </Box>
+
+        <Box width="48%">
+          <Typography variant="h5" color={"#70d8bd"} gutterBottom>
+            Axolot SQLDB Data
+          </Typography>
+          <CustomTable data={sqlDbData} columns={columns} />
+        </Box>
+      </Box>
+
+      {/* <HealthBarChart data={healthMonitorData} /> */}
     </Box>
   );
 };
 
-export default HelathMonitor;
+export default HealthMonitor;
