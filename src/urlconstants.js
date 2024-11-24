86d06f476a96583aa2acfa4a14cc1f6a6ee469b4
sajints <sajints@gmail.com>
2024-11-24 21:12:41 +0100

    URLs

--- src/urlconstants.js
+++ src/urlconstants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:7240";
-// export const API_BASE_URL = "https://axolot.azurewebsites.net";
+// export const API_BASE_URL = "http://localhost:7240";
+export const API_BASE_URL = "https://axolot.azurewebsites.net";
 export const GET_DASHBOARD_URL = "/api/DashboardController";
 export const GET_DEVICES_URL = "/api/GetDevices";
 export const GET_THERAPYLOG_URL = "/api/GetTherapyLogStream";
